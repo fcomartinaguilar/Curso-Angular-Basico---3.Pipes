16379f091c7421d9a515c6a0b944b8aaed67d069
Francisco Martin Aguilar <fcomartinaguilar@gmail.com>
2018-05-18 17:49:51 +0200

    mi primer commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+ nombre = "Curro";
+ nombre2 = "Curro perez LOPez";
+ arreglo = [1,2,3,4,5,6,7,8,9,10];
+ PI = Math.PI;
+ a:number = 0.234;
+ salario:number = 1234.5;
+ heroe = {
+   nombre: "Logan",
+   clave: "Wolverine",
+   edad: 500,
+   direccion:{
+     calle: "Primera",
+     casa: "19"
+   }
+  }
+
+  valorDePromesa = new Promise ( (resolve, reject) => {
+    setTimeout( () => resolve('Llego la data!'), 3500 );
+  })
+  
+  fecha = new Date();
+  vide = "mOeSfOJrUIk";
+  activar:boolean = true;
+}
